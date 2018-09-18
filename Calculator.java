274a0b70e1e0dbd0c7387d35c48171f33c01c969
Lokijopu <kguo2134@gmail.com>
2018-09-17 23:34:15 -0400

    Add files via upload
    Created Calculator and MainCalculator

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+//access modifier, return type, arguments/parameters
+	public int subtract(int num1, int num2) {
+
+		int ans = num1 - num2; //initialize when the method is called
+		
+			return ans;
+			
+	}
+	
+}
+
---
+++ MainCalculator.java
@@ -0,0 +1,10 @@
+
+public class MainCalculator {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator(); 
+		System.out.print("The difference of 10 and 6 is " + calculator.subtract(10, 6) + ".");
+
+	}
+
+}
